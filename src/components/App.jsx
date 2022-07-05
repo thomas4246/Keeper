6cba6e8d060e6fd8e8f937dd9f4c5bf0ee611db7
Chiwen.L <zxcvbnm840508@gmail.com>
2022-07-05 16:43:45 -0700

    add headind & footer and style

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Heading from './Heading';
+import Footer from './Footer';
+import Note from './Note';
+
+function App() {
+  return (
+    <>
+      <Heading />
+      <Note />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
