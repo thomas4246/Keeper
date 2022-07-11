e719d180eac6e041d5d6d8f934c24298f1acd2b9
Chiwen.L <zxcvbnm840508@gmail.com>
2022-07-10 17:56:35 -0700

    Create notes.js & map through

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Heading from './Heading';
 import Footer from './Footer';
 import Note from './Note';
+import notes from '../notes';
 
 function App() {
   return (
     <>
       <Heading />
-      <Note title='First note' content='This is a test note.' />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </>
   );
